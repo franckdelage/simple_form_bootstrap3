2a7ebf3721f39b80e5e4853c845c8b63add13774
web82 <franckweb82@gmail.com>
2014-12-03 14:21:17 +0100

    fix custom datepicker inputs

--- lib/simple_form/inputs/date_time_picker_input.rb
+++ lib/simple_form/inputs/date_time_picker_input.rb
@@ -1,8 +1,10 @@
 module SimpleForm
   module Inputs
     class DateTimePickerInput < Base
-      def input(wrapper_options)
-        @builder.datetime_field(attribute_name, merge_wrapper_options(input_html_options, wrapper_options))
+      def input(wrapper_options = nil)
+        merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
+
+        @builder.datetime_field(attribute_name, merged_input_options)
       end
 
       private
